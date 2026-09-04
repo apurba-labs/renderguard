688cb13a2ba42eb8de786341c618918885786841
Apurba Singh <apurbansinghdev@gmail.com>
2026-09-05 04:24:50 +0600

    chore: prepare pipeline API for Cloud Run

--- pipeline/api/app.py
+++ pipeline/api/app.py
@@ -23,6 +23,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3001",
         "http://127.0.0.1:3001",
+        "https://renderguard.gotihub.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
